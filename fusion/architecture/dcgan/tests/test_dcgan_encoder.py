d20339ac79d42047bb397d06dae0a5457164d1d6
Alex Fedorov <eidos92@gmail.com>
2020-11-23 14:30:46 -0500

    Adjust test for dcgan encoder

--- fusion/architecture/dcgan/tests/test_dcgan_encoder.py
+++ fusion/architecture/dcgan/tests/test_dcgan_encoder.py
@@ -6,15 +6,20 @@ import unittest
 class TestDcganEncoder(unittest.TestCase):
 
     def test_forward(self):
+        # define parameters
         input_size = 32
         dim_in = 1
         dim_h = 2
         dim_l = 4
         dim_cls = [8]
         batch_size = 2
+        # create encoder
         encoder = DcganEncoder(dim_in, dim_h, dim_l, dim_cls, input_size=input_size)
+        # create input
         x = torch.rand(batch_size, dim_in, input_size, input_size)
+        # forward pass
         output = encoder(x)
+        # check outputs
         self.assertEqual(len(output), 2)
         z, latents = output
         self.assertEqual(z.size(0), batch_size)
