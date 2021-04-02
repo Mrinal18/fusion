73dd6f80ffd1554799ae36a323b3d9bd3df03906
Eloy Geenjaar <55933473+eloygeenjaar@users.noreply.github.com>
2021-04-01 21:52:39 -0400

    Update utils.py

--- fusion/architecture/base_block/utils.py
+++ fusion/architecture/base_block/utils.py
@@ -5,43 +5,48 @@ class Flatten(nn.Module):
 
     def __init__(self):
         """
-        Class for receive flatten tensor
-        Return
-            Flatten tensor
+        Custom Pytorch module that flattens an input tensor
+        
+        Return:
+            Flattened tensor
         """
         super(Flatten, self).__init__()
 
     def forward(self, input_tensor):
         """
-        Method for receive flatten tensor
+        The forward function that receives an input tensor and flattens it
+        
         Args:
-            :param input_tensor: Input tensor for flatten
+            :param input_tensor: Input tensor to flatten
         Return:
-            Flatten tensor
+            Flattened tensor
         """
+        # Flatten tensor using .view() to avoid memory copies
         return input_tensor.view(input_tensor.size(0), -1)
 
 
 class Unflatten(nn.Module):
     def __init__(self, input_dim=2):
         """
-        Class for receive unflatten tensor for set dim
+        Custom Pytorch module that unflattens the tensor for a set number of dimensions
+        
         Args:
-            :param input_dim: Input dimension
+            :param input_dim: Input dimension, the input tensor is unsqueezed input_dim times
 
-        Return
-            unflattering tensor
+        Return:
+            Unflattened tensor
         """
         super(Unflatten, self).__init__()
         self.input_dim = input_dim
 
     def forward(self, input_tensor):
         """
-        Method for receive unflattering tensor
+        The forward function that receives a tensor and unflattens it self.input_dim times
+        
         Args:
-            :param input_tensor: Input tensor for unflattering
-        Return
-            unflattering tensor
+            :param input_tensor: Input tensor to unflatten
+        Return:
+            Unflattened tensor
         """
         for _ in range(self.input_dim):
             input_tensor = input_tensor.unsqueeze(-1)
