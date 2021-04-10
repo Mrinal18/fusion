55ae6e6d10c71fed8e6d562e99fa44a632ab10dd
Alex Fedorov <eidos92@gmail.com>
2021-04-09 20:11:13 -0400

    Add docs, add copyright

--- fusion/criterion/loss/rr_ae.py
+++ fusion/criterion/loss/rr_ae.py
@@ -14,6 +14,15 @@ class RR_AE(ABaseLoss):
         self,
         estimator_setting: Setting,
     ):
+        # ToDo: Add references to ABaseMIEstimator
+        """
+        Implementation of the RR-AE loss
+
+        Args:
+            estimator_setting: Setting for Mutual Information estimator. See ABaseMIEstimator for details.
+        Returns:
+            Instance of RR_AE
+        """
         super().__init__()
         self._ae_loss = AE()
         self._rr_loss = MultiDim(
@@ -28,6 +37,16 @@ class RR_AE(ABaseLoss):
         preds: ModelOutput,
         target: Optional[Tensor] = None
     ) -> Tuple[Optional[Tensor], Dict[str, Any]]:
+        """
+        Forward pass for the loss.
+
+        Args:
+            preds: Model output
+            target: Targets, however, loss do no use them.
+        Returns:
+            total_loss: Total loss
+            raw_losses: Dictionary for logging with all the computed losses
+        """
         total_loss = None
         raw_losses = {}
         loss, ae_raw = self._ae_loss(preds)
