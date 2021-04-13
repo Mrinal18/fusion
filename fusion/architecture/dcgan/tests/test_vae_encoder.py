53c938ce0c0385f457f8ed440f40487bf968842c
Eloy Geenjaar <geenjaareloy@gmail.com>
2021-04-13 13:33:45 -0400

    Started files and first naive implementation of VAE encoder

---
+++ fusion/architecture/dcgan/tests/test_vae_encoder.py
@@ -0,0 +1,35 @@
+from fusion.architecture.dcgan import VAEEncoder
+import torch
+import unittest
+
+
+class TestVAEEncoder(unittest.TestCase):
+
+    def test_forward(self):
+        # define parameters
+        input_size = 32
+        dim_in = 1
+        dim_h = 2
+        dim_l = 4
+        dim_cls = [8]
+        batch_size = 2
+        # create encoder
+        encoder = VAEEncoder(dim_in, dim_h, dim_l, dim_cls, input_size=input_size)
+        # create input
+        x = torch.rand(batch_size, dim_in, input_size, input_size)
+        # forward pass
+        output = encoder(x)
+        # check outputs
+        self.assertEqual(len(output), 2)
+        z, latents = output
+        self.assertEqual(z.size(0), batch_size)
+        self.assertEqual(z.size(1), dim_l)
+        for i, (d, l) in enumerate(latents.items()):
+            if d != 1:
+                self.assertEqual(l.size(0), batch_size)
+                self.assertEqual(l.size(-1), dim_cls[i])
+                self.assertEqual(len(l.size()), len(x.size()))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ fusion/architecture/dcgan/vae_autoencoder.py
@@ -0,0 +1,44 @@
+from .vae_encoder import VAEEncoder
+from .dcgan_decoder import DcganDecoder
+from fusion.architecture import ABaseArchitecture
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class VAE(ABaseArchitecture):
+    def __init__(
+        self,
+        prior_dist,
+        likelihood_dist,
+        post_dist,
+        enc: VAEEncoder,
+        dec: DcganDecoder,
+        pz_params,
+    ):
+        super(VAE, self).__init__()
+        self.pz = prior_dist
+        self.px_z = likelihood_dist
+        self.qz_x = post_dist
+        self.enc = enc
+        self.dec = dec
+        self._pz_params = pz_params
+        self._qz_x_params = None
+        self.llik_scaling = 1.0
+
+    @property
+    def pz_params(self):
+        return self._pz_params
+
+    @property
+    def qz_x_params(self):
+        if self._qz_x_params is None:
+            raise NameError("qz_x params not initalised yet!")
+        return self._qz_x_params
+
+    def forward(self, x, K=1):
+        self._qz_x_params = self.enc(x)
+        qz_x = self.qz_x(*self._qz_x_params)
+        zs = qz_x.rsample(torch.Size([K]))
+        px_z = self.px_z(*self.dec(zs))
+        return qz_x, px_z, zs
